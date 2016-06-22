be30c8ac53abc49f8636c51b6880773701141d6f
montythemack <bhill2588@gmail.com>
2016-06-21 23:52:01 -0400

    first commit

---
+++ Mooviez/Mooviez/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System.Web.Mvc;
+
+namespace Mooviez.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(FormCollection collection)
+        {
+            string genre = collection.GetValues("genre").GetValue(0).ToString();
+
+            return View();
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
